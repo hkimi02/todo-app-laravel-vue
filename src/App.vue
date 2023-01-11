b404d0f8c46e2502b66a21e4e16583cd6f2f26e9
hkimi amin <hkimiamin02@gmail.com>
2023-01-11 03:10:33 +0100

    add front end

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+    <nav class="navbar navbar-expand-lg bg-light shadow-lg">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">todoapp</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/about">about</router-link>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
   <router-view/>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(bootstrap).mount('#app');
