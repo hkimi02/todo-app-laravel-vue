af17388dc20e25291afd2732c14685a3b376bdfb
hkimi amin <hkimiamin02@gmail.com>
2023-01-15 16:11:40 +0100

    add

---
+++ src/components/categories.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="categories" >
+        <span class="badge text-bg-primary" v-for="categorie in categories">{{categorie.name}}</span>
+    </div>
+</template>
+<script>
+export default{
+    props:{
+        categories:Array,
+    }
+}
+</script>
+<style scoped>
+.categories{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr ;
+    gap: 10px;
+    overflow-x: auto;
+}
+</style>
