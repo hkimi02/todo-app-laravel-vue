d2ca68decefb94836ea249e67b57fbf97dbae2b8
hkimi amin <hkimiamin02@gmail.com>
2023-01-15 16:11:21 +0100

    add

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,12 @@
           <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="/about">about</router-link>
+          <router-link class="nav-link" to="/about">add task</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/categoriesManager">manage categories</router-link>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
